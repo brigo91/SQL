2bc7d8cec7ff5817fec2d168d5f7866bee5f9449
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-03-11 10:07:19 +0100

    Inserting querying more data

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO users (yearly_salary, full_name, current_status)
+-- VALUES (25000, 'Julie Barnes', 'emloyed')
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (25000, 'Julie Barnes', 'emloyed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (0, 'Michael Smith', 'unemployed');
+
