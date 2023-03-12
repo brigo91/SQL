5ba89f19aacae3e3043d7620e1a7c476b4ae28f6
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-03-12 09:29:50 +0100

    updating table

---
+++ 11-update-tables.sql
@@ -0,0 +1,11 @@
+-- ALTER TABLE employers
+-- ALTER COLUMN yearly_revenue SET DATA TYPE FLOAT; -- Postgre sql
+
+-- ALTER TABLE employers
+-- MODIFY COLUMN yearly_revenue FLOAT;
+
+-- ALTER TABLE users
+-- ALTER COLUMN full_name SET DATA TYPE VARCHAR(300); -- Postgresql
+
+ALTER TABLE users
+MODIFY COLUMN full_name VARCHAR(300);
