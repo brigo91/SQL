fc8909b13c087cf907f7fffaf2215be15956eadd
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-03-22 07:11:54 +0100

    Multiple JOINs

--- 06-normalize/04-inner-join.sql
+++ 06-normalize/04-inner-join.sql
@@ -1,3 +1,4 @@
-SELECT usr.id, first_name, last_name, street, house_number, city_id
+SELECT usr.id, first_name, last_name, street, house_number, c.name AS city_name
 FROM users AS usr
 INNER JOIN addresses AS addr ON usr.address_id = addr.id
+INNER JOIN cities AS c ON addr.city_id = c.id
