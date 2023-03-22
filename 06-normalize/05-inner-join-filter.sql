ea310ba017705d9b0a492d9a041dee53abc8801b
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-03-22 07:20:05 +0100

    Data join & filtering

--- 06-normalize/05-inner-join-filter.sql
+++ 06-normalize/05-inner-join-filter.sql
@@ -1,4 +1,6 @@
 SELECT usr.id, first_name, last_name, street, house_number, c.name AS city_name
 FROM users AS usr
 INNER JOIN addresses AS addr ON usr.address_id = addr.id
-INNER JOIN cities AS c ON addr.city_id = c.id;
+INNER JOIN cities AS c ON addr.city_id = c.id
+WHERE c.id = 1 OR c.id = 2
+ORDER BY usr.id DESC;
