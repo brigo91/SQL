2c1190d4d4ddf6bb61c3074626a2254028965ee3
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-04-08 18:50:52 +0200

    create table

---
+++ 12-summary/02-create-tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE events (
+    id INT,
+    name VARCHAR(300),
+    date_planned TIMESTAMP,
+    image VARCHAR(300),
+    description TEXT,
+    max_participants INT,
+    min_age INT
+);
