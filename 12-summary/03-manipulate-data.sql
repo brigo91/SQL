e620b8dac5abdcbc0d7166e9a8c96c0382790440
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-04-09 09:49:45 +0200

    Query data

--- 12-summary/03-manipulate-data.sql
+++ 12-summary/03-manipulate-data.sql
@@ -7,14 +7,14 @@ INSERT INTO events (
 )
 VALUES(
     'A first event',
-    '2022-10-29 16:30:00'
+    '2022-10-29 16:30:00',
     'This is the description of this first event.',
     20,
     18
 ),
 (
     'A second event',
-    '2022-05-20 12:30:00'
+    '2022-05-20 12:30:00',
     'This is the description of this first event.',
     20,
     22
