7b2a85787780ca872da3029afa09226c1d5f7630
bbrigovacz <balazs.brigovacz@onrobot.com>
2023-04-11 15:50:04 +0200

    ON DELETE and ON UPDATE

--- 12-summary/05-create-locations-tables.sql
+++ 12-summary/05-create-locations-tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE locations (
     street VARCHAR(300) NOT NULL,
     house_number VARCHAR(10) NOT NULL,
     postal_code VARCHAR(5) NOT NULL,
-    city_name VARCHAR(300) REFERENCES cities
+    city_name VARCHAR(300) REFERENCES cities ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE events (
@@ -21,5 +21,5 @@ CREATE TABLE events (
     description TEXT NOT NULL,
     max_participants INT CHECK (max_participants > 0),
     min_age INT CHECK (min_age > 0)
-    location_id INT REFERENCES locations
+    location_id INT REFERENCES locations ON DELETE CASCADE
 );
